034d238746897fcf3118b36e61cf6bd15617bafa
Jon Morley <Jon.Morley@dazn.com>
2021-06-18 15:32:30 +0100

    QAction 1091

---
+++ QAction_1091/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QAction_1091")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("QAction_1091")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6A367ADF-BF83-4E51-B60A-228232B661A5")]
+[assembly: AssemblyVersion("1.0.0.0")]
