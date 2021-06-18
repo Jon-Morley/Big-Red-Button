882acaf1eb185e07c63745d1d5c0c138434ef0ca
Jon Morley <Jon.Morley@dazn.com>
2021-06-18 17:07:59 +0100

    QAction 1091

--- QAction_1091/QAction_1091.cs
+++ QAction_1091/QAction_1091.cs
@@ -12,13 +12,23 @@ public static class QAction
 {
     /// <summary>
     /// The QAction entry point.
-    /// </summary>
-    /// <param name="protocol">Link with SLProtocol process.</param>
-    public static void Run(SLProtocol protocol)
+	/// Process SendPatch return - the "down" json key will flip true/false
+	/// {
+    ///"server": "192.168.48.36:80",
+    ///"weight": 1,
+    ///"max_conns": 0,
+    ///"max_fails": 0,
+    ///"fail_timeout": "10s",
+    ///"slow_start": "0s",
+    ///"route": "",
+    ///"backup": false,
+    //"down": false
+	/// </summary>
+	/// <param name="protocol">Link with SLProtocol process.</param>
+public static void Run(SLProtocol protocol)
     {
         try
         {
-
         }
         catch (Exception ex)
         {
